b6b8620cee0934db47c881bdcbf865b1e446e94a
Rihards Steinbergs <rihards.steinbergs@gmail.com>
2014-10-16 22:25:25 +0300

    Updated the content headers to use the same functions.

--- content-page.php
+++ content-page.php
@@ -1,12 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?>>
 	<header class="entry-header">
-		<?php if (is_single()) : ?>
-		<h1 class="entry-title "><?php the_title(); ?></h1>
-		<?php else : ?>
-		<h1 class="entry-title">
-			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr(sprintf('Permalink to %s', the_title_attribute('echo=0'))); ?>" rel="bookmark"><?php the_title(); ?></a>
-		</h1>
-		<?php endif; ?>
+		<?php
+		if(is_single()) {
+			the_title('<h1 class="entry-title">', '</h1>');
+		}
+		else {
+			the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
+		}
+		?>
 	</header>
 	<div class="entry-content">
 		<?php the_content('Read more'); ?>
--- content-standard.php
+++ content-standard.php
@@ -1,12 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class('post-number-' . $post_number . ' clearfix'); ?>>
 	<header class="entry-header">
-		<?php if (is_single()) : ?>
-		<h1 class="entry-title "><?php the_title(); ?></h1>
-		<?php else : ?>
-		<h1 class="entry-title">
-			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr(sprintf('Permalink to %s', the_title_attribute('echo=0'))); ?>" rel="bookmark"><?php the_title(); ?></a>
-		</h1>
-		<?php endif; ?>
+		<?php
+		if(is_single()) {
+			the_title('<h1 class="entry-title">', '</h1>');
+		}
+		else {
+			the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
+		}
+		?>
 	</header>
 	<div class="post-thumbnail"><?php if(has_post_thumbnail()) { the_post_thumbnail(); } ?></div>
 	<div class="entry-entry">
