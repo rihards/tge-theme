6bed557d82cd8e8dc4548ed2daf2aef3a31a66c1
Rihards Steinbergs <rihards.steinbergs@gmail.com>
2014-10-16 22:36:35 +0300

    Internationalisation!

--- index.php
+++ index.php
@@ -19,20 +19,20 @@
 		<?php endwhile; ?>
 		<div class="navigate-search clearfix">
 			<div class="navigate-left">
-				<?php if(!is_singular() && !is_tag()) { echo '<span class="btn-inactive">' . get_previous_posts_link('Newer') . '</span>'; } ?> &nbsp;
+				<?php if(!is_singular() && !is_tag()) { echo '<span class="btn-inactive">' . get_previous_posts_link(__('Newer', 'tge-theme')) . '</span>'; } ?> &nbsp;
 			</div>
 			<div class="navigate-search-form"><?php get_search_form(); ?></div>
 			<div class="navigate-right">
-				<?php if(!is_singular() && !is_tag()) { echo '<span class="btn-active">' . get_next_posts_link('Older') . '</span>'; } ?> &nbsp;
+				<?php if(!is_singular() && !is_tag()) { echo '<span class="btn-active">' . get_next_posts_link(__('Older', 'tge-theme')) . '</span>'; } ?> &nbsp;
 			</div>
 		</div>
 	<?php else : ?>
 		<article id="post-0" class="post no-results not-found">
 			<header class="entry-header">
-				<h1 class="entry-title">Nothing found</h1>
+				<h1 class="entry-title"><?php _e('Nothing found', 'tge-theme'); ?></h1>
 			</header>
 			<div class="entry-content">
-				<p>Apologies, but no results were found. Perhaps searching will help find a related post.</p>
+				<p><?php _e('Apologies, but no results were found. Perhaps searching will help find a related post.', 'tge-theme'); ?></p>
 				<?php get_search_form(); ?>
 			</div>
 		</article>
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url('/'); ?>">
-	<div><label class="screen-reader-text" for="s">Search for:</label>
-		<input type="text" value="" name="s" id="s" placeholder="Search" />
-		<input type="submit" id="searchsubmit" value="Search" />
+	<div><label class="screen-reader-text" for="s"><?php _e('Search for:', 'tge-theme'); ?></label>
+		<input type="text" value="" name="s" id="s" placeholder="<?php _e('Search', 'tge-theme'); ?>" />
+		<input type="submit" id="searchsubmit" value="<?php _e('Search', 'tge-theme'); ?>" />
 	</div>
 </form>
