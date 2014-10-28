b9bd87f6d90e39d7f2e3c531477b02dc5e9760ee
Rihards Steinbergs <rihards@exove.fi>
2014-10-28 20:30:15 +0200

    Basic favicon support and icon itself.

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
 <title><?php bloginfo('name'); ?> <?php wp_title('|', true, 'left'); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
