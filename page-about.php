07f99e6fc995c0b6a9a0f3439458a5d9cdb7fa1d
Rihards Steinbergs <rihards@exove.fi>
2014-10-28 22:19:34 +0200

    About page template doesn't need h1 link.

--- page-about.php
+++ page-about.php
@@ -3,14 +3,7 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-		if(is_single()) {
-			the_title('<h1 class="entry-title">', '</h1>');
-		}
-		else {
-			the_title('<h1 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h1>');
-		}
-		?>
+		<?php the_title('<h1 class="entry-title">', '</h1>');?>
 	</header>
 
 	<div class="entry-entry clearfix">
