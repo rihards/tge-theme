76d135794ca031b84d2ab94f8f52c36f92df64c6
Rihards Steinbergs <rihards@exove.fi>
2014-11-17 18:16:04 +0200

    Added a config.rb so we can use compass.

---
+++ config.rb
@@ -0,0 +1,13 @@
+relative_assets = true
+css_dir = "."
+sass_dir = "_sass"
+images_dir = "images"    #the folder with your images
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded # After dev :compressed
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+
+# Obviously
+preferred_syntax = :scss
