95c7179f187928ec2925f8bd1d68b0badb6b7c18
Rihards Steinbergs <rihards@exove.fi>
2014-12-03 16:24:18 +0200

    Updates to search form phrasing.

--- searchform.php
+++ searchform.php
@@ -2,7 +2,15 @@
 	<div class="clearfix">
     <label class="screen-reader-text" for="s"><?php _e('Search for:', 'tge-theme'); ?></label>
     <div class="search-icon"><?php echo tge_icon('search'); ?></div>
-		<input type="text" value="" name="s" id="s" placeholder="<?php _e('type here', 'tge-theme'); ?>" />
+		<input
+      type="text"
+      value=""
+      name="s"
+      id="s"
+      placeholder="<?php _e('Type here', 'tge-theme'); ?>"
+      onfocus="this.placeholder = ''"
+      onblur="this.placeholder = '<?php _e('Type here', 'tge-theme'); ?>'"
+    />
 		<input type="submit" id="searchsubmit" value="<?php _e('Search', 'tge-theme'); ?>" />
 	</div>
 </form>
